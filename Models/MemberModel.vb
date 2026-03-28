bb35e9eb59d33ca5e27a765887f0eaa6d2bc49fb
168bbs-ship-it <168bbs@gmail.com>
2026-03-28 19:21:08 +0800

    Create MemberModel class with member properties

---
+++ Models/MemberModel.vb
@@ -0,0 +1,10 @@
+Public Class MemberModel
+    Public Property Id As Integer
+    Public Property Name As String
+    Public Property Email As String
+    Public Property PhoneNumber As String
+    Public Property Address As String
+    Public Property DateOfBirth As DateTime
+    Public Property MembershipDate As DateTime
+    Public Property IsActive As Boolean
+End Class
